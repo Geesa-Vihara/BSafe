90ae1bc91b468ffbdeb52cd174213b3690a3a41f
Geesa-Vihara <geesavihara97@gmail.com>
2020-06-20 03:06:43 +0530

    Listing paired devices

--- android/app/src/main/java/com/bsafe/BluetoothModule.java
+++ android/app/src/main/java/com/bsafe/BluetoothModule.java
@@ -1,5 +1,15 @@
 package com.bsafe;
  
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.Set;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.Callback;
@@ -8,6 +18,11 @@ import com.facebook.react.uimanager.IllegalViewOperationException;
  
 
 public class BluetoothModule  extends ReactContextBaseJavaModule  {
+    
+    private ListView lstvw;
+    private ArrayAdapter aAdapter;
+    private BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
+
     public BluetoothModule(ReactApplicationContext reactContext) {
         super(reactContext); //required by React Native
     }
@@ -22,7 +37,23 @@ public class BluetoothModule  extends ReactContextBaseJavaModule  {
     @ReactMethod
     public void scanBluetoothDevices(Callback errorCallback, Callback successCallback) {
         try {
-            successCallback.invoke("Send number of scanned devices");
+            if(bAdapter==null){
+                System.out.println("Bluetooth Not Supported");
+            }
+            else{
+                Set<BluetoothDevice> pairedDevices = bAdapter.getBondedDevices();
+                ArrayList list = new ArrayList();
+                String res="";
+                if(pairedDevices.size()>0){
+                    for(BluetoothDevice device: pairedDevices){
+                        String devicename = device.getName();
+                        String macAddress = device.getAddress();
+                        list.add("Name: "+devicename+"MAC Address: "+macAddress);
+                        res=res+"Name: "+devicename+"MAC Address: "+macAddress+"\n";
+                    }                    
+                }
+                successCallback.invoke("Send number of scanned devices "+list.size()+"\n"+res);
+            }
         } catch (IllegalViewOperationException e) {
             errorCallback.invoke(e.getMessage());
         }
